49030b1c10cab4e0590a75bfadc7f3cda0abd3e6
subhajitdeo <subhajitdeoghoria5@gmail.com>
2026-05-13 20:45:49 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 let offscreenCreated = false;
+let offscreenReady = false;
 
 async function createOffscreen() {
   if (offscreenCreated) return;
@@ -14,21 +15,42 @@ async function closeOffscreen() {
   if (offscreenCreated) {
     await chrome.offscreen.closeDocument();
     offscreenCreated = false;
+    offscreenReady = false;
   }
 }
 
+// Listen for messages from offscreen
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === 'wakeWordHeard') {
+  if (message.type === 'offscreenReady') {
+    offscreenReady = true;
+    // If always-listen was enabled, tell offscreen to start
+    chrome.storage.local.get(['alwaysListen'], (result) => {
+      if (result.alwaysListen === true && offscreenReady) {
+        chrome.runtime.sendMessage({ type: 'startListening' }).catch(e => console.warn(e));
+      }
+    });
+    sendResponse({});
+  }
+  else if (message.type === 'wakeWordHeard') {
     console.log('Wake word "Zara" detected');
-  } else if (message.type === 'voiceCommand') {
+  }
+  else if (message.type === 'voiceCommand') {
     chrome.storage.local.set({ pendingVoiceCommand: message.command });
     chrome.action.openPopup();
-  } else if (message.type === 'setAlwaysListen') {
+  }
+  else if (message.type === 'setAlwaysListen') {
     const enabled = message.enabled;
     chrome.storage.local.set({ alwaysListen: enabled });
     if (enabled) {
-      createOffscreen();
+      createOffscreen().then(() => {
+        if (offscreenReady) {
+          chrome.runtime.sendMessage({ type: 'startListening' }).catch(e => console.warn(e));
+        }
+      });
     } else {
+      if (offscreenReady) {
+        chrome.runtime.sendMessage({ type: 'stopListening' }).catch(e => console.warn(e));
+      }
       closeOffscreen();
     }
     sendResponse({ success: true });
@@ -57,6 +79,7 @@ chrome.commands.onCommand.addListener((command) => {
   }
 });
 
+// On extension startup, restore always-listen state
 chrome.storage.local.get(['alwaysListen'], (result) => {
   if (result.alwaysListen === true) {
     createOffscreen();
